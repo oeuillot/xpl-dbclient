ae87b6c3be85c5f710084dbbecb469deb8e202ad
Olivier Oeuillot <oeuillot@vedana.com>
2016-06-16 17:17:16 +0200

    Add test

---
+++ test.js
@@ -0,0 +1,29 @@
+/*jslint node: true, vars: true, nomen: true, esversion: 6 */
+'use strict';
+
+const commander = require('commander');
+const debug = require('debug')('xpl-dbclient');
+
+const XplDBClient = require('./API');
+const Memcache = XplDBClient.Memcache;
+const Query = XplDBClient.Query;
+
+commander.version(require("./package.json").version);
+
+Memcache.fillCommander(commander);
+
+commander.command("request").action((path) => {
+  var query=new Query(commander);
+  
+  query.getValue(path, (error, value) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    
+    console.log(value);
+    
+    query.close();
+  });
+  
+});
